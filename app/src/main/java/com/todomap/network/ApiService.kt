311f8977aaecff70ef1fc3b9c1b7b0ff6d6aedbd
William <ome450901@gmail.com>
2020-04-23 00:15:46 +1000

    Set up retrofit

---
+++ app/src/main/java/com/todomap/network/ApiService.kt
@@ -0,0 +1,32 @@
+package com.todomap.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://mars.udacity.com/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface ApiService {
+    @GET("realestate")
+    fun getPropertiesAsync(): Deferred<String>
+}
+
+object Api {
+    val RETROFIT_SERVICE: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
